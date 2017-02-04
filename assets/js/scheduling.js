00f0e290dfdeb1bfb6e68a4f6f8334245b632a3c
JP Rodrigues <joaorodrigues@infojr.com.br>
2017-02-03 22:25:52 -0300

    Permite escalonamento com valores decimais
    Refs #2

--- assets/js/scheduling.js
+++ assets/js/scheduling.js
@@ -1,4 +1,8 @@
-function newJobInfo(element) {
+/**
+ * Generates new group of inputs to register a job
+ * @return {void}
+ */
+function newJobInfo() {
   var count = parseInt($('#plus').attr('value')) || 1;
   $('#job .container').append('\
     <div class="job-info">\
@@ -14,32 +18,93 @@ function newJobInfo(element) {
   $('#plus').attr('value', count);
 }
 
+/**
+ * Mounts an array with the inputted values
+ * @param  {event} event JS event to execute preventDefault
+ * @return {Array}
+ */
 function mountArray(event) {
   event.preventDefault();
   $('.job').remove();
   var arr = [];
 
   for (var i = 0; i < $('.count').length; i++) {
-    exec = $('#execution-' + i).val();
-    arrival = $('#arrival-' + i).val();
-    dist = arrival;
-    arr.push({execution: exec.toString(), off: arrival.toString(), distance: dist.toString()});
+    exec = parseFloat($('#execution-' + i).val());
+    arrival = parseFloat($('#arrival-' + i).val());
+
+    arr.push({execution: exec, off: arrival, distance: arrival});
   }
 
   return arr;
 }
 
-function SortByArrival(a, b){
-  var aArrival = a.off.toLowerCase();
-  var bArrival = b.off.toLowerCase();
-  return ((aArrival < bArrival) ? -1 : ((aArrival > bArrival) ? 1 : 0));
-}
-
+/**
+ * Returns an ascii table element from 65
+ * @param  {integer} i  an integer to be added to 65
+ * @return {String}     ascii charater
+ */
 function getJob(i) {
   var job = 65 + i;
   return String.fromCharCode(job);
 }
 
+/**
+ * Returns a HTML element that represents the job
+ * @param  {float} execution Job execution time
+ * @param  {float} wait      Job Wait time
+ * @param  {float} distance  Time between 0 and job execution start
+ * @param  {float} off       Time between job arrival time and job execution start
+ * @param  {integer} i       iterate value
+ * @return {String}          HTML Element that represents the job
+ */
+function mountHtml(execution, wait, distance, off, i) {
+  eFloat = getDecimal(execution);
+  execution = getInteger(execution);
+  wFloat = getDecimal(wait);
+  wait = getInteger(wait);
+  dFloat = getDecimal(distance);
+  distance = getInteger(distance);
+  oFloat = getDecimal(off);
+  off = getInteger(off);
+
+  return '<dd class="job execution-' + execution + ' e-float-' + eFloat + ' wait-' + wait + ' w-float-' + wFloat + ' distance-' + distance + ' d-float-' + dFloat + ' off-' + off + ' o-float-' + oFloat + '"><span class="text">' + getJob(i) + '</span></dd>';
+}
+
+/**
+ * Returns number's integer part
+ * @param  {float} value number to be explored
+ * @return {string}
+ */
+function getInteger(value) {
+  return Math.floor(value).toString();
+}
+
+/**
+ * Return number's decimal part
+ * @param  {float} value number to be explored
+ * @return {string}
+ */
+function getDecimal(value) {
+  return Math.floor(value % 1 * 100).toString();
+}
+
+/**
+ * Argument to sort by Arrival
+ * @param {number} a element to be compared
+ * @param {number} b element to be compared
+ * @return {number}
+ */
+function SortByArrival(a, b){
+  var aArrival = a.off;
+  var bArrival = b.off;
+  return ((aArrival < bArrival) ? -1 : ((aArrival > bArrival) ? 1 : 0));
+}
+
+/**
+ * Scheduling by Arrival
+ * @param  {Array} arr  Job Array
+ * @return {void}
+ */
 function fifo(arr) {
   arr.sort(SortByArrival);
 
@@ -48,16 +113,16 @@ function fifo(arr) {
 
     if (arr[i].execution != 0) {
 
-      if (i > 0) diff = (parseInt(arr[i].distance) + parseInt(arr[i - 1].distance) + parseInt(arr[i - 1].execution) - parseInt(arr[i].off)).toString();
+      if (i > 0) diff = (arr[i].distance + arr[i - 1].distance + arr[i - 1].execution - arr[i].off);
 
-      if (diff > parseInt(arr[i].distance)) arr[i].distance = diff;
+      if (diff > arr[i].distance) arr[i].distance = diff;
+
+      wait = (arr[i].distance - arr[i].off);
+
+      if (wait < 0) wait = 0;
 
 
-      wait = (parseInt(arr[i].distance) - parseInt(arr[i].off)).toString();
-      if (parseInt(wait) < 0) wait = '0';
-
-
-      $('#fifo').append('<dd class="job execution-' + arr[i].execution + ' wait-' + wait + ' distance-' + arr[i].distance + ' off-' + arr[i].off + '"><span class="text">' + getJob(i) + '</span></dd>');
+      $('#fifo').append(mountHtml(arr[i].execution, wait, arr[i].distance, arr[i].off, i));
     }
   }
 }
